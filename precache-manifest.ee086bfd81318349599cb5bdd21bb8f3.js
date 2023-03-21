26cc780b3dac70bf4b6916b8428b53a2e4ae8f21
ujjwalpandeyjava <52150001+ujjwalpandeyjava@users.noreply.github.com>
2023-03-21 07:44:36 +0000

    Deploying to gh-pages from @ ujjwalpandeyjava/Ujjwal-Portfolio@34be87dbaca6f0f091a97b15ce3a719dc409f837 🚀

---
+++ precache-manifest.ee086bfd81318349599cb5bdd21bb8f3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "c10f4b39fdd2447c6320",
+    "url": "/Ujjwal-Portfolio/static/css/main.491270e8.chunk.css"
+  },
+  {
+    "revision": "c10f4b39fdd2447c6320",
+    "url": "/Ujjwal-Portfolio/static/js/main.c10f4b39.chunk.js"
+  },
+  {
+    "revision": "00baaf3cb7bec74b6e53",
+    "url": "/Ujjwal-Portfolio/static/js/1.00baaf3c.chunk.js"
+  },
+  {
+    "revision": "a1c92c8d120beb18f14a",
+    "url": "/Ujjwal-Portfolio/static/js/2.a1c92c8d.chunk.js"
+  },
+  {
+    "revision": "2aaf5261d566dcb896e5",
+    "url": "/Ujjwal-Portfolio/static/js/runtime~main.2aaf5261.js"
+  },
+  {
+    "revision": "7a19cf0da2b22a2a17b992d163b9db43",
+    "url": "/Ujjwal-Portfolio/static/media/arrow-down.7a19cf0d.png"
+  },
+  {
+    "revision": "cc3c95bf5bc517ae146308567905c659",
+    "url": "/Ujjwal-Portfolio/static/media/ujjwalAvatar3.cc3c95bf.png"
+  },
+  {
+    "revision": "a47ef3e7e5babf05766a65d6470423a2",
+    "url": "/Ujjwal-Portfolio/static/media/Ujjwal_Pandey_Resume.a47ef3e7.pdf"
+  },
+  {
+    "revision": "6357e4a1d79c13be9773e0da7b195c1e",
+    "url": "/Ujjwal-Portfolio/static/media/Ujjwal_Pandey_C_V.6357e4a1.pdf"
+  },
+  {
+    "revision": "1b62b2aa01b32b0c6730d0638be17826",
+    "url": "/Ujjwal-Portfolio/static/media/resumePreviewImage.1b62b2aa.png"
+  },
+  {
+    "revision": "ce43eed3fe83408f4c08c04dc1b45955",
+    "url": "/Ujjwal-Portfolio/static/media/cvPreviewImage.ce43eed3.png"
+  },
+  {
+    "revision": "7382e4834ba6ced59b496abedb33350b",
+    "url": "/Ujjwal-Portfolio/static/media/contact.7382e483.png"
+  },
+  {
+    "revision": "b8681c4a5f23dfd3ae09690017c71095",
+    "url": "/Ujjwal-Portfolio/static/media/ujjwalAvatarEducation.b8681c4a.png"
+  },
+  {
+    "revision": "6df50e22d681dfc874740f7c1ce3a5d3",
+    "url": "/Ujjwal-Portfolio/static/media/IMG-20210325-WA0006.6df50e22.jpg"
+  },
+  {
+    "revision": "4165d1b7f1bff2e027f56d9929ff85d0",
+    "url": "/Ujjwal-Portfolio/static/media/ujjwalAvatar1Copy.4165d1b7.png"
+  },
+  {
+    "revision": "689a9f41e24a54808d6e4dd2af285e10",
+    "url": "/Ujjwal-Portfolio/static/media/projectPic.689a9f41.png"
+  },
+  {
+    "revision": "0f9c022d090737e4fdeb0ea68bd155f8",
+    "url": "/Ujjwal-Portfolio/static/media/AdvancedJava.0f9c022d.jfif"
+  },
+  {
+    "revision": "c9f82da1d1bc74cbf7d78cb35a63f9c2",
+    "url": "/Ujjwal-Portfolio/static/media/dsalog.c9f82da1.jfif"
+  },
+  {
+    "revision": "275c8374fd65712aa0722f3adf3b2ee0",
+    "url": "/Ujjwal-Portfolio/static/media/etc.275c8374.jfif"
+  },
+  {
+    "revision": "094effdfe9fa7436755a230988f83126",
+    "url": "/Ujjwal-Portfolio/static/media/Google_Cloud.094effdf.png"
+  },
+  {
+    "revision": "0aec6149126718fc12130ea0ef3b7df7",
+    "url": "/Ujjwal-Portfolio/static/media/git.0aec6149.jfif"
+  },
+  {
+    "revision": "02e1adfb5d3fbc1ea5f2057704be84ef",
+    "url": "/Ujjwal-Portfolio/static/media/js.02e1adfb.jpg"
+  },
+  {
+    "revision": "b0061610011592ffbd1c1a3eb35342ba",
+    "url": "/Ujjwal-Portfolio/static/media/msOffice.b0061610.jfif"
+  },
+  {
+    "revision": "5f8ab490a2e8cc938a13f3f6396939c4",
+    "url": "/Ujjwal-Portfolio/static/media/ReactJS.5f8ab490.jpg"
+  },
+  {
+    "revision": "fceaf0bfdcd5c908c8c02d2533c22e45",
+    "url": "/Ujjwal-Portfolio/static/media/restAPI.fceaf0bf.jfif"
+  },
+  {
+    "revision": "dc3728c37316a98428b4a437882edf6a",
+    "url": "/Ujjwal-Portfolio/static/media/spring1.dc3728c3.jpg"
+  },
+  {
+    "revision": "0c01ce252e0e0c8be3c437db62b1cd57",
+    "url": "/Ujjwal-Portfolio/static/media/eclipse_IDE.0c01ce25.jfif"
+  },
+  {
+    "revision": "2a3a861ef834274d97996353598d0330",
+    "url": "/Ujjwal-Portfolio/index.html"
+  }
+];
