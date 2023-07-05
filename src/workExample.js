8297b5eb07312d359fbfde4b6dc2c949d1a5377f
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-07-05 20:40:51 +0530

    New Powerful CV!

--- src/workExample.js
+++ src/workExample.js
@@ -10,24 +10,9 @@ export default function WorkExample(props) {
 				<div id="firstInExamples">
 					<div><img src={projectsPic} alt="Me with certificate" /></div>
 					<div id="aboutExamples" className='container' >
-						{/* <p>Work Examples<br /><sub><i>( Hover/Touch here )</i></sub></p>
-						<div className="overlay" id="overlayID">
-							<div className="details">
-								Click eye to see all the projects in one project "links".
-								<div className="eye">
-									<a href="https://ujjwalpandeyjava.github.io/Links/examples.html"
-										target="_blank" rel="noopener noreferrer">
-										<i className="far fa-eye"></i>
-									</a>
-								</div>
-							</div>
-						</div> */}
-						<Iframe src="https://ujjwalpandeyjava.github.io/Links/examples.html" width="100%" height="600" ></Iframe>
-						{/* <Iframe src="http://127.0.0.1:5501/examples.html" width="100%" height="650" ></Iframe> */}
-
+						<Iframe src="https://ujjwalpandeyjava.github.io/Links/examples.html" title="Projects by me" width="100%" height="600" ></Iframe>
 					</div>
 				</div>
-				{/* <ProfileSummary /> */}
 			</div>
 		</Fragment>
 	)
