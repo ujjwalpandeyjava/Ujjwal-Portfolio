1606e3aa690235a1b1a725968255de12d7ec140a
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-08-25 15:00:08 +0530

    removed one of the two resume preview

--- src/previewPDFs.js
+++ src/previewPDFs.js
@@ -1,18 +1,8 @@
 // Change this component to work on json links (Dynamic), so that it can be used in other areas too like: Present all 'Certificates'
-
 import React, { Fragment } from 'react';
 import previewCSS from './previews.module.css';
 import downArrow2 from './images/arrow-down.png';
 
-// Fresher
-// import UjjwalCV from './images/Resume_CV/fresher/Ujjwal_Pandey_C_V.pdf';
-// import cvPreview from './images/Resume_CV/fresher/cvPreviewImage.png';
-// import UjjwalResume from './images/Resume_CV/fresher/Ujjwal_Pandey_Resume.pdf';
-// import resumePreview from './images/Resume_CV/fresher/resumePreviewImage.png';
-
-// 1.5 yr experienced
-import UjjwalResume from './images/Resume_CV/exp_1_5yr/ujjwal_Pandey_Resume.pdf';
-import resumePreview from './images/Resume_CV/exp_1_5yr/resumePreviewImage.jpeg';
 import UjjwalCV from './images/Resume_CV/exp_1_5yr/ujjwal_Pandey_C_V.pdf';
 import cvPreview from './images/Resume_CV/exp_1_5yr/cvPreviewImage.jpeg';
 
@@ -22,11 +12,11 @@ export default function PreviewPDFs(props) {
 			<div id="previews" className={previewCSS.previewsFullPage} onClick={() => props.viewPreview(false)}>
 				<div className={previewCSS.previewssection} onClick={(e) => e.stopPropagation()}>
 					<div className={previewCSS.closeBtn} onClick={() => props.viewPreview(false)}>X</div>
-					<div>
+					{/* <div>
 						<div><img className={previewCSS.previewImage} src={resumePreview} alt="Resume preview" /></div>
 						<div>Resume in pdf format <img src={downArrow2} style={{ height: '18px', width: '20px' }} alt="Down arrow" /></div>
 						<div className="downloadCV"><a href={UjjwalResume} download='_Ujjwal-full_stack-Resume.pdf'>Download Resume</a></div>
-					</div>
+					</div> */}
 					<div>
 						<div><img className={previewCSS.previewImage} src={cvPreview} alt="C.V. preview" /></div>
 						<div></div>
