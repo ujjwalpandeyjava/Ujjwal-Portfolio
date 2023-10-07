5229da62105479d5313022fcfd2477062241b302
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-10-07 21:04:39 +0530

    Good

--- src/tiltEffect/summaryTilt.js
+++ src/tiltEffect/summaryTilt.js
@@ -144,7 +144,7 @@ export default function SummaryTilt(props) {
 											<path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
 											<circle cx="12" cy="10" r="3" />
 										</svg>
-										Devli, South Delhi, New Delhi, 110080
+										Devli, New Delhi, Delhi
 									</div>
 									<div className="card-contact">
 										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
