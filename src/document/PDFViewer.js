9679f6b633b8f596c5c994fd93f392c915d03c87
Ujjwal <ujjwalpandey.aps@gmail.com>
2024-03-16 02:08:44 +0530

    Fixed pdf previously, was not visible on the UI

--- src/document/PDFViewer.js
+++ src/document/PDFViewer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FiDownload } from "react-icons/fi";
 import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 import { Document, Page, pdfjs } from "react-pdf";
@@ -6,19 +6,12 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import style from './PDFViewer.module.css';
 import pdfFile from '../images/Resume_CV/ujjwalpandey_CV_SDE-II.pdf';
-
-// https://github.com/wojtekmaj/react-pdf#support-for-annotations
-// https://drive.google.com/file/d/1pBi221O_ccRc2Eg9UUKaUZt_yLIzjQV1/view?usp=sharing
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-	"pdfjs-dist/build/pdf.worker.min.js",
-	import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function PDFViewer() {
-	// const fileUrl = 'https://drive.google.com/uc?id=1pBi221O_ccRc2Eg9UUKaUZt_yLIzjQV1';
 	const [numPages, setNumPages] = useState(1);
 	const [pageNumber, setPageNumber] = useState(1);
+
 	const onDocumentLoadSuccess = ({ numPages }) => {
 		setNumPages(numPages);
 	};
@@ -46,6 +39,8 @@ export default function PDFViewer() {
 		</div>
 	);
 };
+
+
 function DownloadCVButton({ pdfFile, extraStyle, text, extraText }) {
 	return (
 		<>
@@ -61,7 +56,7 @@ function PDF({ file, onDocumentLoadSuccess, pageNumber }) {
 	return (
 		<div className={style.page}>
 			<div className={style.doc} >
-				<Document file={file + "d"} onLoadSuccess={onDocumentLoadSuccess} error={<DownloadCVButton extraStyle={{ marginBlock: "8px" }} pdfFile={pdfFile} text="Download C.V." extraText="Browser not supported, please Download PDF!" />}>
+				<Document file={file } onLoadSuccess={onDocumentLoadSuccess} error={<DownloadCVButton extraStyle={{ marginBlock: "8px" }} pdfFile={pdfFile} text="Download C.V." extraText="Browser not supported, please Download PDF!" />}>
 					<Page pageNumber={pageNumber} renderTextLayer={false} />
 				</Document>
 			</div>
