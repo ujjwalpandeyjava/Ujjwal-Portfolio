394c35bf561e8f72c2390f85d18d4c7db5c50138
Ujjwal <ujjwalpandey.aps@gmail.com>
2024-04-15 22:58:01 +0530

    Changed instagram link.

--- src/document/PDFViewer.js
+++ src/document/PDFViewer.js
@@ -5,7 +5,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import style from './PDFViewer.module.css';
-import pdfFile from '../Resume_CV/ujjwalpandey_CV_SDE-II.pdf';
+import pdfFile from '../Resume_CV/_Ujjwal-full_stack-C.V.pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function PDFViewer() {
