07e02cfe1661fe7b3de11e87d5287c0d6f336b10
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2025-01-05 14:39:17 +0530

    New menu access way - Now access is from the sidebar

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,16 +1,20 @@
 import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
-import { BiSolidBarChartAlt2 } from "react-icons/bi";
-import { FaWindowClose } from "react-icons/fa";
-import { Link, useLocation } from 'react-router-dom';
+import { BiSolidBarChartAlt2 } from 'react-icons/bi';
+import { FaWindowClose } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import ujjwalIcon from '../images/ujjwalAvatar3.png';
-import navbarCSS from './Navbar.module.css';
+import style from './Navbar.module.css';
 
 
 export default function Navbar() {
 	const [floatNav, setFloatNav] = useState(false);
-	const [showSmallScreenMenu, setShowSmallScreenMenu] = useState(false);
+	const [showSidebar, setShowSidebar] = useState(true)
 
+	const toggleShowSidebar = () => setShowSidebar(old => !old)
+	const linkClicked = () => toggleShowSidebar()
+
+
+	// Let the navbar float when scrolled
 	useEffect(() => {
 		const handleScroll = () => { setFloatNav(window.scrollY > 80) }
 
@@ -18,40 +22,26 @@ export default function Navbar() {
 		return () => window.removeEventListener('scroll', handleScroll)
 	}, []);
 
-	const currentUrl = useLocation().pathname;
-	const isActive = (event, passedUrl) => {
-		if (currentUrl === passedUrl)
-			toast("Already at " + event.currentTarget.textContent, {
-				icon: '📃🕸️',
-				style: {
-					borderRadius: '10px',
-					background: '#ff9966',
-					color: 'white',
-					padding: '1px'
-				}
-			}
-			);
-	}
 
 	return (
-		<div className={[navbarCSS.navbar, (floatNav) ? navbarCSS.floatingNav : ""].join(" ")} id={navbarCSS.navbar}>
-			<div className={navbarCSS.noHamburger}>
-				<Link to="" onClick={(event) => isActive(event, "/")}>Home</Link>
-				<Link to="./experience" onClick={(event) => isActive(event, "/experience")}>Experience</Link>
-				<Link to="./skills_projects" onClick={(event) => isActive(event, "/skills_projects")}>ShowCase</Link>
-				<Link to="./contact" onClick={(event) => isActive(event, "/contact")}>Contact</Link>
+		<>
+
+			<div id={style.navbar} className={floatNav && style.floatingNav} >
+				<BiSolidBarChartAlt2 className={style.action} size={30} onClick={toggleShowSidebar} />
+				<Link to="/"><img id={style.navIcon} src={ujjwalIcon} alt="It's me, on home" /></Link>
 			</div>
-			<div className={navbarCSS.hamburger}>
-				{showSmallScreenMenu ?
-					<div className={[navbarCSS.sidebar, navbarCSS.move].join(" ")} onClick={() => setShowSmallScreenMenu(false)}>
-						<Link relative='path' to="" onClick={(event) => isActive(event, "/")}>Home</Link>
-						<Link relative='path' to="./experience" onClick={(event) => isActive(event, "/experience")}>Experience</Link>
-						<Link relative='path' to="./skills_projects" onClick={(event) => isActive(event, "/skills_projects")}>ShowCase</Link>
-						<Link relative='path' to="./contact" onClick={(event) => isActive(event, "/contact")}>Contact</Link>
-					</div> : null}
-				{showSmallScreenMenu ? <FaWindowClose size={30} onClick={() => setShowSmallScreenMenu(false)} /> : <BiSolidBarChartAlt2 className={navbarCSS.rotate90} size={30} onClick={() => setShowSmallScreenMenu(true)} />}
+
+			<div className={`${style.sideBar} ${showSidebar && style.hideSidebar}`}>
+				<div className={style.actions}>
+					<div className={style.myName}>Ujjwal Pandey</div>
+					<FaWindowClose className={style.action} size="2rem" onClick={toggleShowSidebar} />
+				</div>
+				<Link className={style.links} onClick={linkClicked} to="">Home</Link>
+				<Link className={style.links} onClick={linkClicked} to="./experience">Experience</Link>
+				<Link className={style.links} onClick={linkClicked} to="./skills_projects">ShowCase</Link>
+				<Link className={style.links} onClick={linkClicked} to="./contact">Contact</Link>
 			</div>
-			<Link to="" onClick={(event) => isActive(event, "/")}><img id={navbarCSS.navIcon} src={ujjwalIcon} alt="It's me, on home" /></Link>
-		</div>
+
+		</>
 	)
 }
